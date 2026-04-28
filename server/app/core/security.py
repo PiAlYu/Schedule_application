f146d350c5318f5f205200d46160b91fffc9b7ad
aypir <aypirogov@gmail.com>
2026-04-28 23:11:18 +0300

    Third fix

--- server/app/core/security.py
+++ server/app/core/security.py
@@ -6,7 +6,10 @@ from passlib.context import CryptContext
 
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["pbkdf2_sha256"],
+    deprecated="auto",
+)
 
 
 class TokenPayloadError(ValueError):
